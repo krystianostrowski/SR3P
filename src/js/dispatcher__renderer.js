9b35b8ba6c3d82ca4d2e976590349ab2ee5c31eb
krystianostrowski <krystian.ostrowski@karatepostomino.pl>
2020-03-05 21:09:49 +0100

    General improvements

--- src/js/dispatcher__renderer.js
+++ src/js/dispatcher__renderer.js
@@ -1,20 +1,18 @@
 const { remote, ipcRenderer } = require('electron');
 
-window.onload = () =>{
-    document.querySelector("#min-btn").addEventListener('click', () => {
-        remote.getCurrentWindow().minimize();
-    });
-    
-    document.querySelector("#max-btn").addEventListener('click', () => {
-        const currentWindow = remote.getCurrentWindow();
-        if (currentWindow.isMaximized()) {
-            currentWindow.unmaximize();
-        } else {
-            currentWindow.maximize();
-        }
-    });
+document.querySelector("#min-btn").addEventListener('click', () => {
+    remote.getCurrentWindow().minimize();
+});
 
-    document.querySelector("#close-btn").addEventListener('click', () => {
-        remote.app.quit();
-    });
-}
+document.querySelector("#max-btn").addEventListener('click', () => {
+    const currentWindow = remote.getCurrentWindow();
+    if (currentWindow.isMaximized()) {
+        currentWindow.unmaximize();
+    } else {
+        currentWindow.maximize();
+    }
+});
+
+document.querySelector("#close-btn").addEventListener('click', () => {
+    remote.app.quit();
+});
